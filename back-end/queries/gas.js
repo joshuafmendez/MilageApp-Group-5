d4b92ca04ec97894fa63c9a23579332f4025b21b
DamienYule <damienyule@gmail.com>
2021-09-01 22:04:10 -0400

    completed backend

--- back-end/queries/gas.js
+++ back-end/queries/gas.js
@@ -10,9 +10,8 @@ const getAllGas = async (car_id) => {
   }
 };
 
-const getGas = async (id,) => {
-  //add car_id maybe. On second thought I dont think it is nessasary and I think Mashu said it wasn't nessasary
-  try {
+const getGas = async (id) => {
+   try {
     const query = "SELECT * FROM gas WHERE id=$1";
     const gas = await db.one(query, id);
     return { status: true, payload: gas };
@@ -21,8 +20,8 @@ const getGas = async (id,) => {
   }
 };
 
-const addGas = async (gas) => {
-  const { car_id, business_use, gas_spent, date } = gas;
+const addGas = async (body, car_id) => {
+  const { business_use, gas_spent, date } = body;
   try {
 
     const query =
@@ -49,7 +48,7 @@ const updateGas = async (id, car) => {
   try {
     const query =
 
-      "UPDATE gas SET car_id,=$1, business_use=$2, gas_spent=$3, date=$4 WHERE id=$5 RETURNING *";
+      "UPDATE gas SET car_id=$1, business_use=$2, gas_spent=$3, date=$4 WHERE id=$5 RETURNING *";
     const updatedGas = await db.one(query, [
       car_id,
       business_use,
