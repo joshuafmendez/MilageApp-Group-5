2c31f349cd03a0cc1c5fb58f30e15ce9bffd634a
joshuamendez <joshuafmendez@gmail.com>
2021-09-18 11:03:24 -0400

    push working code with redux

--- front-end-react/src/Components/CarDetails.js
+++ front-end-react/src/Components/CarDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams, useHistory, Link } from "react-router-dom";
 import axios from "axios";
 import { apiURL } from "../util/apiURL";
@@ -10,7 +10,7 @@ import { UserContext } from "../Providers/UserProvider";
 const API = apiURL();
 
 function CarDetails() {
-  const user = useContext(UserContext)
+  const user = useContext(UserContext);
   let [car, setCar] = useState({});
   // let [userMounted,setUserMounted] = useState(false)
   let { id } = useParams();
@@ -31,24 +31,21 @@ function CarDetails() {
   };
 
   useEffect(() => {
-  //  setTimeout(()=>{},100)
+    //  setTimeout(()=>{},100)
     const fetchCar = async () => {
-     
-      console.log("fetchCarfunction user",user)
+      console.log("fetchCarfunction user", user);
       try {
         let res = await axios.get(`${API}/cars/${id}`);
-        if(res.data.payload.uid === user.uid){
-          
+        if (res.data.payload.uid === user.uid) {
           setCar(res.data.payload);
-          console.log(res.data.payload)
+          console.log(res.data.payload);
         }
       } catch (err) {
         console.log(err);
       }
     };
     fetchCar();
-  
-  }, [id,user]);
+  }, [id, user]);
 
   const { make, model, vin, year, odometer, doors } = car;
 
