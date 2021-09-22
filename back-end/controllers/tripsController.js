aac1a3584cf684a17a722c89d8548293ded8665c
DamienYule <damienyule@gmail.com>
2021-09-21 21:32:01 -0400

    auth complete

--- back-end/controllers/tripsController.js
+++ back-end/controllers/tripsController.js
@@ -12,32 +12,39 @@ const {
 } = require("../queries/trips");
 
 trips.get("/", async (req, res) => {
-  const allTrips = await getAllTrips(req.params.car_id);
+  const { car_id } = req.params;
+  const uid = req.query.uid;
+  const allTrips = await getAllTrips(car_id, uid);
   res.json(allTrips);
 });
 
 trips.get("/:id", async (req, res) => {
-  const trip = await getTrip(req.params.id);
+  const { car_id, id } = req.params;
+  const uid = req.query.uid;
+  const trip = await getTrip(id, car_id, uid);
   res.json(trip);
 });
 
 trips.post("/", async (req, res) => {
   const { body, params } = req;
   const { car_id } = params;
-  const trip = await addTrip(body, car_id);
+  const uid = req.query.uid;
+  const trip = await addTrip(body, car_id, uid);
   res.json(trip);
 });
 
 trips.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const trip = await deleteTrip(id);
+  const uid = req.query.uid;
+  const { id, car_id } = req.params;
+  const trip = await deleteTrip(id, car_id, uid);
   res.json(trip);
 });
 
 trips.put("/:id", async (req, res) => {
   const { body, params } = req;
   const { id } = params;
-  const trip = await updateTrip(id, body);
+  const uid = req.query.uid;
+  const trip = await updateTrip(id, body, uid);
   res.json(trip);
 });
 
