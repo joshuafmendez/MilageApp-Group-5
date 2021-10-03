5214f582810a0242ab9664c03f31f897bab3c615
joshuamendez <joshuafmendez@gmail.com>
2021-10-03 19:06:04 -0400

    fixed unknown cors issue

--- front-end-react/src/Components/NavBar.js
+++ front-end-react/src/Components/NavBar.js
@@ -1,17 +1,8 @@
-import { useParams, useHistory, Link } from "react-router-dom";
-import React, { useEffect, useContext } from "react";
+import { useHistory, Link } from "react-router-dom";
 import "../Components/Style/NavBar.css";
 import { signOut } from "../Services/Firebase";
-import { useDispatch } from "react-redux";
-import { UserContext } from "../Providers/UserProvider";
-import { getAllExpensesFN, getAllTripsFN } from "../util/networkRequest";
-import { addExpenses } from "../Store/Actions/expenseActions";
-import { addTrips } from "../Store/Actions/tripsActions";
 
 export default function NavBar() {
-  const user = useContext(UserContext);
-  const dispatch = useDispatch();
-  let { id } = useParams();
   let history = useHistory();
 
   const handleLogout = async () => {
@@ -22,32 +13,6 @@ export default function NavBar() {
       console.log(error);
     }
   };
-
-  useEffect(() => {
-    const getAllExpenses = async () => {
-      try {
-        if (user) {
-          let res = await getAllExpensesFN(id, user);
-          dispatch(addExpenses(res));
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getAllExpenses();
-
-    const getAllTrips = async () => {
-      try {
-        if (user) {
-          let res = await getAllTripsFN(id, user);
-          dispatch(addTrips(res));
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getAllTrips();
-  }, [id, user, history, dispatch]);
 
   return (
     <div className="log-start">
