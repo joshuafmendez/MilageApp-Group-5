9000c0d3026b2811e97bd859553e0d71c9750df2
DamienYule <damienyule@gmail.com>
2021-10-05 19:19:15 -0400

    some small changes

--- front-end-react/src/Components/Cars/DetailComps/CenterPanel.js
+++ front-end-react/src/Components/Cars/DetailComps/CenterPanel.js
@@ -4,21 +4,21 @@ import "./CenterPanel.css";
 const CenterPanel = ({ id, handleDelete }) => {
   return (
     <div className="concar-div">
-      <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Warning!!!</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLongTitle">Warning!!!</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">
-              You are about to delete this car. We do not archive your records. All records associated with this car will be lost.
+            <div className="modal-body">
+              You are about to delete this car. All records associated with this car will be lost.
             </div>
-            <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal">Keep Car</button>
-              <button type="button" class="btn btn-danger" onClick={handleDelete} data-dismiss="modal">Delete Car forever</button>
+            <div className="modal-footer">
+            <button type="button" className="btn btn-primary" data-dismiss="modal">Keep Car</button>
+              <button type="button" className="btn btn-danger" onClick={handleDelete} data-dismiss="modal">Delete Car</button>
             </div>
           </div>
         </div>
